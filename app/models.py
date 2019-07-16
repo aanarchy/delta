86a01431e04e42b5ca2763089c47d5e3c523ef4a
BuildTools <unconfigured@null.spigotmc.org>
2019-07-16 19:32:37 +0800

    Client multi connections implemented.

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime, default=datetime.now())
     status = db.Column(db.String(32), default='Logged out')
 
     def __repr__(self):
